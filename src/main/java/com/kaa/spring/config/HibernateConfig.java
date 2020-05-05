8864872aed53bc500ea0bae1a5ae54c304b3d235
artemkochergin <kaa777@gmail.com>
2020-05-04 20:39:07 -0400

    Initial commit

---
+++ src/main/java/com/kaa/spring/config/HibernateConfig.java
@@ -0,0 +1,11 @@
+package com.kaa.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ImportResource({ "classpath:persistence.xml" })
+public class HibernateConfig {
+}
